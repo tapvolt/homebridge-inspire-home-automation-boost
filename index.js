fc67915429e537cc5fdd2e421c1c44e2d28af18e
Gareth Jackson <gareth.jackson@gmail.com>
2015-12-17 21:13:47 +0000

    Work in progress

---
+++ index.js
@@ -0,0 +1,58 @@
+var request = require("request");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+
+	homebridge.registerAccessory("homebridge-thermostat", "Thermostat", ThermostatAccessory);
+}
+
+function ThermostatAccessory(log, config) {	
+	this.log = log;
+	this.config = config;	
+	this.name = config["name"];
+
+	this.service = new Service.Thermostat(this.name);
+	this.service
+		.getCharacteristic(Characteristic.On)
+		// .on("get", this.getOn.bind(this))
+		.on("set", this.setOn.bind(this));
+}
+
+ThermostatAccessory.prototype.getOn = function(callback) {
+	request.post({
+		url: "https://www.inspirehomeautomation.co.uk/client/api1_3_1/api.php",
+		formData: {
+			action:
+			apikiey:
+			key:
+			device_id:
+			message:
+			value: 
+		}
+	}, function(error, response, body) {
+		var status = "";
+		callback(null, status);
+	}.bind(this));
+}
+
+ThermostatAccessory.prototype.setOn = function(on, callback) {
+	var input = on ? 1 : 0;
+	request.post({
+		url: "https://www.inspirehomeautomation.co.uk/client/api1_3_1/api.php",
+		formData: {
+			action: "send_message",
+
+			message: "set_function",
+			value: input
+		}
+	}, function(error, response, body) {
+		var status = "";
+		callback(null, status);
+	}.bind(this));
+}
+
+ThermostatAccessory.prototype.getServices = function() {
+	return [this.service];
+}
