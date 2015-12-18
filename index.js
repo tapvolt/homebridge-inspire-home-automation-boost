36e4b7fdaeceaca56513319397b4099813156b95
Gareth Jackson <gareth.jackson@gmail.com>
2015-12-18 14:24:34 +0000

    Initial commit

--- index.js
+++ index.js
@@ -3,47 +3,37 @@ var parseString = require('xml2js').parseString;
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
 
-	homebridge.registerAccessory(
-		'homebridge-inspire-home-automation', 
-		'Thermostat', 
-		ThermostatAccessory
-	);
+    homebridge.registerAccessory(
+        'homebridge-inspire-home-automation-boost',
+        'Boost',
+        BoostAccessory
+    );
 }
 
-function ThermostatAccessory(log, config) {		
-	this.endpoint = {
-		url: 'https://www.inspirehomeautomation.co.uk/client/api1_3_1/api.php',  
-		header: {
-     		'cache-control': 'no-cache',
-     		'content-type': 'multipart/form-data;' 
-     	}   	
-	};
-	this.config = config;	
-	this.name = config['name'];
-	
-	this.log = log;
-	this.log('Inspire Home Automation Thermostat');
-	this.log('Device Id:', this.config.device_id);
-	
-	this.service = new Service.Thermostat(this.name);		
-	this.service.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-		.on('get', this.getCurrentHeatingCoolingState.bind(this));
-	this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState)
-		.on('get', this.getTargetHeatingCoolingState.bind(this))
-		.on('set', this.setTargetHeatingCoolingState.bind(this));
-	this.service.getCharacteristic(Characteristic.CurrentTemperature)
-		.on('get', this.getCurrentTemperature.bind(this));
-	this.service.getCharacteristic(Characteristic.TargetTemperature)
-		.on('get', this.getTargetTemperature.bind(this))
-		.on('set', this.setTargetTemperature.bind(this));
-	this.service.getCharacteristic(Characteristic.TemperatureDisplayUnits)
-		.on('get', this.getTemperatureDisplayUnits.bind(this));
+function BoostAccessory(log, config) {
+    this.endpoint = {
+        url: 'https://www.inspirehomeautomation.co.uk/client/api1_3_1/api.php',
+        header: {
+            'cache-control': 'no-cache',
+            'content-type': 'multipart/form-data;'
+        }
+    };
+    this.config = config;
+    this.name = config['name'];
+    this.log = log;
+    this.log('Inspire Home Automation Thermostat - Boost!');
+    this.log('Device Id:', this.config.device_id);
+
+    this.service = new Service.Switch(this.name);
+
+    this.service.getCharacteristic(Characteristic.On)
+        .on('set', this.setBoost.bind(this));
 
     /*
-     We are only switching between On and Off
+     Inspire Home Automation API
      1 = Off
      2 = Program 1
      3 = Program 2
@@ -52,22 +42,16 @@ function ThermostatAccessory(log, config) {
      6 = Boost
      */
     this.setFunctionHelper = function(on) {
-        var state = on == 1 ? 5 : 1;
+        this.log('Call for Boost', on);
+        var state = 6;
         this.log('Inspire Home Automation set_function value', state);
         return state;
     }
-}
-
-ThermostatAccessory.prototype.getCurrentHeatingCoolingState = function(callback) {
 
 }
 
-ThermostatAccessory.prototype.getTargetHeatingCoolingState = function(callback) {
-
-}
-
-ThermostatAccessory.prototype.setTargetHeatingCoolingState = function(on, callback) {	
-	this.log('setTargetHeatingCoolingState', on);
+BoostAccessory.prototype.setBoost = function(on, callback) {
+	this.log('setBoost', on);
 	var options = { 
 		method: 'POST',
   		url: this.endpoint.url,
@@ -87,64 +71,6 @@ ThermostatAccessory.prototype.setTargetHeatingCoolingState = function(on, callba
 	}.bind(this));
 }
 
-ThermostatAccessory.prototype.getCurrentTemperature = function(callback) {
-	this.log('getCurrentTemperature')
-	var options = { 
-		method: 'GET',
-  		url: this.endpoint.url,
-  		headers: this.endpoint.header,
-  		qs: { 
-  			action: 'get_device_information',
-     		apikey: this.config.apikey,
-     		key: this.config.key,
-     		device_id: this.config.device_id
-     	} 
-    };
-    request(options, function(error, response, body) {
-            var json;
-            parseString(body,toString(), function (err, result) {
-                json = JSON.parse(
-                    JSON.stringify(result)
-                );
-            });
-            this.log(json.xml.Device_Information[0].Current_Temperature[0]);
-            callback(null, parseFloat(json.xml.Device_Information[0].Current_Temperature[0]));
-    }.bind(this));
-}
-
-ThermostatAccessory.prototype.getTargetTemperature = function(callback) {
-
-}
-
-ThermostatAccessory.prototype.setTargetTemperature = function(temp, callback) {
-    this.log('setTargetTemperature', temp);
-    var options = {
-        method: 'POST',
-        url: this.endpoint.url,
-        headers: this.endpoint.header,
-        formData: {
-            action: 'send_message',
-            apikey: this.config.apikey,
-            key: this.config.key,
-            device_id: this.config.device_id,
-            message: 'set_set_point',
-            value_temp: Math.floor(temp)
-        }
-    };
-    request(options, function(error, response, body) {
-        this.log(body);
-        callback(null, temp);
-    }.bind(this));
-}
-
-ThermostatAccessory.prototype.getTemperatureDisplayUnits = function(callback) {	
-    this.log('getTemperatureDisplayUnits');
-    var units = Characteristic.TemperatureDisplayUnits.CELSIUS;
-    var unitsName = units == Characteristic.TemperatureDisplayUnits.FAHRENHEIT ? 'Fahrenheit' : 'Celsius';
-    this.log('Temperature unit for ' + this.name + ' is: ' + unitsName);
-    callback(null, units);
-}
-
-ThermostatAccessory.prototype.getServices = function() {
+BoostAccessory.prototype.getServices = function() {
 	return [this.service];
 }
